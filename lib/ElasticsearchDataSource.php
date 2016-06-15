361bf200cd0d943f64a232a0bf707f0465d07c8f
Adam Franco <afranco@middlebury.edu>
2016-06-15 17:13:30 -0400

    Can now search for wildcard indices using the Field Stats API.
    This is how new versions of Kibana 4 work, removing the need to make assumptions
    about the naming convention of indices.
    
    https://www.elastic.co/guide/en/elasticsearch/reference/2.3/search-field-stats.html

--- lib/ElasticsearchDataSource.php
+++ lib/ElasticsearchDataSource.php
@@ -19,6 +19,7 @@ class ElasticsearchDataSource {
 	protected $base_url;
 	protected $index_base;
 	protected $verbose = FALSE;
+	protected $index_cache = array();
 
 	/**
 	 * Constructor
@@ -52,6 +53,93 @@ class ElasticsearchDataSource {
 	}
 
 	/**
+	 * Answer an array of indices in which to search.
+	 *
+	 * @param int $from Timestamp to begin searching at.
+	 * @param mixed $to Timestamp to end searching at or 'now'.
+	 * @return array
+	 */
+	protected function getIndices($from, $to) {
+		// Use the field-stats API to search for indices if we have a wild-card.
+		// This is the method used by Kibana > 4.1.
+		// https://www.elastic.co/guide/en/elasticsearch/reference/2.3/search-field-stats.html
+		if (strpos($this->index_base, '*') !== FALSE) {
+			return $this->getIndicesBySearch($from, $to);
+		}
+		// Make assumptions about the naming scheme of indices.
+		else {
+			return $this->getIndicesByPattern($from, $to);
+		}
+	}
+
+	/**
+	 * Answer an array of indices in which to search.
+	 *
+	 * Use the field-stats API to search for indices if we have a wild-card.
+	 * This is the method used by Kibana > 4.1.
+	 * https://www.elastic.co/guide/en/elasticsearch/reference/2.3/search-field-stats.html
+	 *
+	 * @param int $from Timestamp to begin searching at.
+	 * @param mixed $to Timestamp to end searching at or 'now'.
+	 * @return array
+	 */
+	protected function getIndicesBySearch($from, $to) {
+		if (!isset($this->index_cache[$from.'-'.$to])) {
+			$url = $this->base_url.$this->index_base.'/_field_stats?level=indices&pretty=true';
+			$request_data = json_encode(array(
+				'fields' => array('@timestamp'),
+				'index_constraints' => array(
+					'@timestamp' => array(
+						"max_value" => array(
+							"gte" => date('c', $from),
+						),
+						"min_value" => array(
+							"lte" => date('c', $to),
+						),
+					),
+				),
+			));
+			$message = @http_post_data($url, $request_data, $options, $info);
+			if (empty($message)) {
+				$error = $info['error'];
+				$error .= ' total_time='.$info['total_time'];
+				$error .= ' namelookup_time='.$info['namelookup_time'];
+				$error .= ' connect_time='.$info['connect_time'];
+				$error .= ' pretransfer_time='.$info['pretransfer_time'];
+				$error .= ' num_connects='.$info['num_connects'];
+				$error .= ' os_errno='.$info['os_errno'];
+				throw new Exception('HTTP POST to '.$url.' failed. '.$error);
+			}
+			$response = http_parse_message($message);
+			$result = json_decode($response->body, TRUE);
+			$this->index_cache[$from.'-'.$to] = array_keys($result['indices']);
+			if ($this->verbose) {
+				print "Search for indices at $url with \n\t$request_data\nfound\n\t".implode(', ', $this->index_cache[$from.'-'.$to])."\n";
+			}
+		}
+		return $this->index_cache[$from.'-'.$to];
+	}
+
+	/**
+	 * Answer an array of indices in which to search.
+	 *
+	 * Assume regular date structure for the index names.
+	 *
+	 * @param int $from Timestamp to begin searching at.
+	 * @param mixed $to Timestamp to end searching at or 'now'.
+	 * @return array
+	 */
+	protected function getIndicesByPattern($from, $to) {
+		$indices = array();
+		$indices[] = $this->index_base.'-'.gmdate('Y.m.d', $to);
+		$indices[] = $this->index_base.'-'.gmdate('Y.m.d', $from);
+		for ($t = $from + 24 * 3600; $t < $to; $t = $t + 24 * 3600) {
+			$indices[] = $this->index_base.'-'.gmdate('Y.m.d', $t);
+		}
+		return $indices;
+	}
+
+	/**
 	 * Perform a search
 	 *
 	 * @param mixed $data
@@ -75,12 +163,7 @@ class ElasticsearchDataSource {
 		}
 
 		// Add an index for each day covered in the search time, will be filtered as unique.
-		$indices = array();
-		$indices[] = $this->index_base.'-'.gmdate('Y.m.d', $to);
-		$indices[] = $this->index_base.'-'.gmdate('Y.m.d', $from);
-		for ($t = $from + 24 * 3600; $t < $to; $t = $t + 24 * 3600) {
-			$indices[] = $this->index_base.'-'.gmdate('Y.m.d', $t);
-		}
+		$indices = $this->getIndices($from, $to);
 		$indices = array_unique($indices);
 		sort($indices);
 
@@ -119,7 +202,14 @@ class ElasticsearchDataSource {
 				}
 				if (!empty($result->error) || $info['response_code'] != 200) {
 					if (!empty($result->error))
-						$message = $result->error;
+						if (is_object($result->error)) {
+							$message = $result->error->type.": ".$result->error->reason;
+							if (!empty($result->error->index)) {
+								$message .= " in index ".$result->error->index;
+							}
+						} else {
+							$message = $result->error;
+						}
 					else
 						$message = '';
 					throw new Exception('Search to '.$info['effective_url'].' failed with response_code '.$info['response_code'].'. '.$message);
